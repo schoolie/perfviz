259e85a22d82b3ddfaf26d277540ac7e3c517224
schoolie <brian.p.schoolcraft@gmail.com>
2015-10-20 20:41:07 -0400

    moved install script

--- perfviz/install_geos.sh
+++
@@ -1,8 +0,0 @@
-sudo yum install git-core boost-devel gcc-c++
-
-cd ..
-wget http://geos.refractions.net/downloads/geos-3.0.0.tar.bz2
-tar -jxvf geos-3.0.0.tar.bz2
-cd geos-3.0.0
-./configure
-sudo make install
